844deb6f1953b1c3fbc2db3ce7e7dbf7cd32c0bd
irinaog <goncharirina08@gmail.com>
2022-06-29 13:53:01 +0300

    task 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Profile } from 'components/Profile/Profile';
+import user from './user.json'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+    />
   </React.StrictMode>
 );
