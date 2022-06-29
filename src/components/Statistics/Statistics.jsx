8c82c4d56124ec5b778d59e11bc60f1d19ea08d5
irinaog <goncharirina08@gmail.com>
2022-06-29 22:48:52 +0300

    task 2

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,29 +1,26 @@
 import PropTypes from 'prop-types';
-// import css from './Profile.module.css';
+import css from './Statistics.module.css';
 
-export const Statistics = ({title, stats}) => {
-    return (
-        <section class="statistics">
-  <h2 class="title">Upload stats</h2>
 
-  <ul class="stat-list">
-    <li class="item">
-      <span class="label">.docx</span>
-      <span class="percentage">4%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp3</span>
-      <span class="percentage">14%</span>
-    </li>
-    <li class="item">
-      <span class="label">.pdf</span>
-      <span class="percentage">41%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp4</span>
-      <span class="percentage">12%</span>
-    </li>
-  </ul>
-</section>
-    )
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul className={css.statList}>
+        {stats.map(item => (
+          <li key={item.id} className={css.item}>
+          <span className={css.label}>{item.label}</span>
+          <span className={css.percentage}>{item.percentage}%</span>
+        </li>
+        ))}
+      </ul>
+    </section>
+  )
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.shape),
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Profile } from 'components/Profile/Profile';
-import user from './user.json'
+import { Statistics } from 'components/Statistics/Statistics';
+import user from './user.json';
+import data from './data.json';
 import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,5 +18,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       views={user.stats.views}
       likes={user.stats.likes}
     />
+    
+    <Statistics
+      title="Upload stats"
+      stats={data} />
+  
   </React.StrictMode>
+  
 );
