3eb2fca51e60aeb7e7b7c4f322b87615e0854f77
irinaog <goncharirina08@gmail.com>
2022-07-02 11:45:10 +0300

    bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,35 @@
-// export const App = () => {
-//   return (<div
-//     style={{
-//       background: 'red',
-//       height: '200px',
-//     }}>
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+import transaction from '../transactions.json';
 
-//   </div>
-//   );
-// };
+export const App = () => {
+    return (
+        <>
+            <Profile
+                username={user.username}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                followers={user.stats.followers}
+                views={user.stats.views}
+                likes={user.stats.likes}
+            />
+    
+            <Statistics
+                title="Upload stats"
+                stats={data} />
+    
+            <FriendList
+                friends={friends} />
+    
+            <TransactionHistory
+                items={transaction} />
+        </>
+    )
+};
 
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//     </div>
-//   );
-// };
--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Profile } from 'components/Profile/Profile';
-import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
-import user from './user.json';
-import data from './data.json';
-import friends from './friends.json';
-import transaction from './transactions.json';
+
 import './index.css';
+import { App } from 'components/App';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-    />
-    
-    <Statistics
-      title="Upload stats"
-      stats={data} />
-    
-    <FriendList
-      friends={friends} />
-    
-    <TransactionHistory
-    items={transaction}/>
+    <App/>
   
   </React.StrictMode>
   
